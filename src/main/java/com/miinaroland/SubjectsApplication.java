3d1aa7d54b48bf97aa729a1df81fd9d17f48aeb1
Roland Talvar <talvar.roland@gmail.com>
2016-05-21 16:01:38 +0300

    Initial commit

---
+++ src/main/java/com/miinaroland/SubjectsApplication.java
@@ -0,0 +1,12 @@
+package com.miinaroland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/miinaroland/SubjectsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.miinaroland;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SubjectsApplication.class)
+@WebAppConfiguration
+public class SubjectsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
